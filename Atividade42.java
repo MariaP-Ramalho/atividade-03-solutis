318d782961b85ededf8b7a12b08e3f10e679d995
MariaP-Ramalho <eduardapramalho@gmail.com>
2024-08-19 17:33:39 -0300

    refactor: change the names of the variables

--- Atividade42.java
+++ Atividade42.java
@@ -4,25 +4,25 @@ import java.util.Random;
 public class Atividade42 {
     private static final int MIN = 0;
     private static final int MAX = 59;
-    private static final int NUM_GRUPOS = 40;
-    private static final int TAMANHO_GRUPO = 3;
+    private static final int NUM_GROUPS = 40;
+    private static final int SIZE_GROUPS = 3;
 
     public static void main(String[] args) {
         Random random = new Random();
 
-        System.out.println("Lista de " + NUM_GRUPOS + " grupos de números sorteados de " + MIN + " a " + MAX + ":");
+        System.out.println("Lista de " + NUM_GROUPS + " grupos de números sorteados de " + MIN + " a " + MAX + ":");
 
-        for (int i = 0; i < NUM_GRUPOS; i++) {
-            int[] grupo = new int[TAMANHO_GRUPO];
+        for (int i = 0; i < NUM_GROUPS; i++) {
+            int[] group = new int[SIZE_GROUPS];
 
-            for (int j = 0; j < TAMANHO_GRUPO; j++) {
-                grupo[j] = random.nextInt(MAX - MIN + 1) + MIN;
+            for (int j = 0; j < SIZE_GROUPS; j++) {
+                group[j] = random.nextInt(MAX - MIN + 1) + MIN;
             }
 
 
-            Arrays.sort(grupo);
+            Arrays.sort(group);
 
-            System.out.println("Grupo " + (i + 1) + ": " + Arrays.toString(grupo));
+            System.out.println("Grupo " + (i + 1) + ": " + Arrays.toString(group));
         }
     }
 }
